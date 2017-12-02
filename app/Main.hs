5be94558f10e2e8e83f8ec890bc9c6d2fe5d2363
EricFalkenberg <exf4789@rit.edu>
2017-12-01 20:54:05 -0800

    Add simple HTTP server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Lib
+import Control.Monad
+import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, dir, seeOther)
 
 main :: IO ()
-main = someFunc
+main = simpleHTTP nullConf $ msum 
+    [ dir "hello" $ ok "Hello, World!",
+      dir "bye"   $ ok "Goodbye, World!",
+      seeOther "/hello" "/hello" 
+    ]
+
