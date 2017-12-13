f58605a6e8c2d7c7418a398d07a6ee531b40ee29
EricFalkenberg <exf4789@rit.edu>
2017-12-12 20:13:33 -0800

    Experiment with GET/POST

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,20 @@
 module Main where
 
 import Lib
-import Control.Monad
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, dir, seeOther)
+import Control.Monad (msum)
+import Happstack.Server (Method(GET, POST)
+                        , method
+                        , nullConf
+                        , simpleHTTP
+                        , toResponse
+                        , ok
+                        , dir)
 
 main :: IO ()
 main = simpleHTTP nullConf $ msum 
-    [ dir "hello" $ ok "Hello, World!",
-      dir "bye"   $ ok "Goodbye, World!",
-      seeOther "/hello" "/hello" 
+    [ do method GET
+         ok $ "You did a GET request.\n"
+    , do method POST
+         ok $ "You did a POST request.\n"
     ]
 
